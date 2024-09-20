7adb35aa47f984b79045dea26337efd9d795e769
Geferson Lopes <gefersonjefreey@gmail.com>
2024-09-20 01:01:03 -0300

    feat: update props in button

--- app/components/Button/Button.tsx
+++ app/components/Button/Button.tsx
@@ -5,14 +5,16 @@ interface ButtonProps {
   onClick?: () => void;
   children?: React.ReactNode;
   className?: string;
+  type?: "button" | "submit" | "reset" | undefined;
 }
 
-const Button: React.FC<ButtonProps> = ({ onClick, children, className }) => {
+const Button: React.FC<ButtonProps> = ({ onClick, children, className, type }) => {
   const classes = className?.split(" ").map((cls) => styles[cls]);
   return (
     <button
       className={`${styles.button} ${classes && classes.join(" ")}`}
       onClick={onClick}
+      type={type ? type : "button"}
     >
       {children}
     </button>
